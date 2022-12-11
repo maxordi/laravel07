4662b70cab21b466ac4939cc1d249da990b4eafb
maxordi <max.ordashevskiy@yandex.by>
2022-12-11 18:20:41 +0300

    laravel homework

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::factory()->count(20)->create([
+            'img' => fake()->imageUrl('400','400')
+        ]);
+    }
+}
