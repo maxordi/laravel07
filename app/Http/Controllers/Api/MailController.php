a5d16f9504e0ee8293f9e733210141e1ac535bfb
maxordi <max.ordashevskiy@yandex.by>
2023-01-22 14:40:18 +0300

    Console Game

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Interfaces\ICurrencyInterface;
+use App\Jobs\ProcessPodcast;
 use App\Mail\HelloMail;
 use App\Models\User;
+use App\Services\NbrbBank;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 
 class MailController extends Controller
 {
-    public function index(){
-        $users = User::all();
-        foreach ($users as $user){
-            Mail::to($user)->send(new HelloMail());
-        }
+    public function index(Request $request, ICurrencyInterface $bank){
+
+        $bank->getAllCurrenciesOnDate('dddd');
+        $bank->getCurrency('sd','dsg');
+
+        $value = Cache::remember('users', 100, function () {
+            return User::all();
+        });
+
+
+        return $value;
+
+//        $users = User::all();
+//        foreach ($users as $user){
+//            ProcessPodcast::dispatch($user)->onQueue('emails');
+//        }
     }
 }
